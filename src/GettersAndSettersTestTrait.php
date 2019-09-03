8149ac574858c4f36f39c549765f755af243aa46
Oleksii Zymovets <oleksii.zymovets@amadeus.com>
2019-09-03 12:01:05 +0300

    added basic helper for testing entities

---
+++ src/GettersAndSettersTestTrait.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Cybalex\TestHelpers;
+
+use PHPUnit\Framework\MockObject\MockBuilder;
+
+Trait GettersAndSettersTestTrait
+{
+    /**
+     * @var object
+     */
+    protected $entity;
+
+    public function setUp(): void
+    {
+        /** @var MockBuilder $mockBuilder */
+        $mockBuilder = $this->getMockBuilder($this->getEntityClass());
+        $this->entity = $mockBuilder
+            ->addMethods($this->getMockedMethods())
+            ->setConstructorArgs($this->getConstructorArguments())
+            ->getMock()
+        ;
+    }
+
+    /**
+     * Data provider for testing getter and setter methods of an entity
+     *
+     * Usage example:
+     * return [
+     *   ['name', 'John'],
+     * ];
+     * Result: this will test the following:
+     *   1/ $entity->setName('John')  will return the instance of $entity (chain call test)
+     *   2/ $entity->getName() will return 'John'
+     *
+     * @return array
+     */
+    abstract function gettersAndSettersDataProvider(): array;
+
+    abstract function getEntityClass();
+
+    /**
+     * @dataProvider gettersAndSettersDataProvider
+     * @param string $propertyName
+     * @param mixed $value
+     */
+    public function testGettersAndSetters(string $propertyName, $value)
+    {
+        $setter = 'set'.ucfirst($propertyName);
+        $getter = 'get'.ucfirst($propertyName);
+
+        $this->assertSame($this->entity, $this->entity->$setter($value));
+        $this->assertEquals($value, $this->entity->$getter());
+    }
+
+    /**
+     * Override this method to pass constructor arguments to entity
+     *
+     * @return array
+     */
+    protected function getConstructorArguments(): array
+    {
+        return [];
+    }
+
+    /**
+     * Example:
+     * Override this method to mock methods of entity
+     * return ['getName']
+     * getName method of entity will be mocked
+     *
+     * @return array
+     */
+    protected function getMockedMethods(): array
+    {
+        return [];
+    }
+}
